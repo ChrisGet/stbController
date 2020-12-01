ef55a09dd48f0cb3612260ce439ead4697ebce1c
Chris Get <christopher.get@sky.uk>
2020-12-01 10:29:56 +0000

    Update changelog

--- scripts/initialSetup.pl
+++ scripts/initialSetup.pl
@@ -32,42 +32,28 @@ system("chmod 777 $titlefile");
 system("chmod -R 777 $configdir");
 
 my $arch = '';
-my $dotnetfile = '';
 chomp(my $archraw = `uname -m` // '');
 if ($archraw) {
 	if ($archraw =~ /x86/) {
 		if ($archraw =~ /64/) {
 			$arch = 'x86_64';
-			$dotnetfile = 'dotnet-sdk-3.1.402-linux-x64.tar.gz';
 		} else {
 			print "WARNING: RedRatHub is not supported on Linux 32 bit. You will not be able to use IrNetBoxIV hardware on this host.\n";
-			#$arch = 'x86_32';
 		}
 	} elsif ($archraw =~ /arm/) {
 		if ($archraw =~ /v(\d+)/) {
 			my $ver = $1;
 			if ($ver < 8) {
 				$arch = 'arm32';
-				$dotnetfile = 'dotnet-sdk-3.1.402-linux-arm.tar.gz';
 			} else {
 				$arch = 'arm64';
-				$dotnetfile = 'dotnet-sdk-3.1.402-linux-arm64.tar.gz';
 			}
 		}
 	}
 }
 
-if ($arch and $dotnetfile) {
+if ($arch) {
 	my $dir = $maindir . 'dotnet' . $arch;
-	#chomp(my $installcheck = `ls -1 $dir \| grep LICENSE`);
-	#if (!$installcheck) {
-	#	my $file = "$dir/$dotnetfile";
-	#	print "Architecture successfully found. Will use $dir\nUnpacking dotnet files, please wait...\n";
-	#	#system("cd $dir && tar -xf $file");
-	#	#print "dotnet files unpacked. Finished\n";
-	#} else {
-	#	print "The correct version of dotnet already appears to be extracted\nFinished\n";
-	#}
 	system("echo $arch > $archfile && chmod 777 $archfile");
 }
 
